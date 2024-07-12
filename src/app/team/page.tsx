5f7d569669d7cf5bd824580f69e4ed1ec0545558
Missie Dawes <missiedawes@gmail.com>
2024-07-12 01:46:35 +0000

    Update team page styles and add Logan's headshot

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -15,6 +15,7 @@ export default function About() {
             name: "Logan Sauers",
             role: "The Ninja",
             shortBio: "Logan pursues perfection with the exactness of a master swordsman, seeking answers with the patience of a monk in meditation.",
+            photoUrl: "/logan-sauers.jpg",
             slug: "logan-sauers",
         },
         {
@@ -37,19 +38,23 @@ export default function About() {
     return (
         <>
             <h1 className="text-3xl font-semibold pb-6">Meet Our Team</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="gap-4 grid grid-cols-1 lg:grid-cols-2">
                 {teamMembers.map((member) => (
-                    <div key={member.name} className="bg-neutral-800 p-4 rounded-lg">
-                        {member.photoUrl &&
-                            <Image src={member.photoUrl} alt={member.name} width={200} height={200} className="rounded-full mr-auto ml-auto mb-4" />
-                        }
-                        <div className="flex">
-                            <div>
-                                <h2 className="text-xl font-semibold">{member.name}</h2>
-                                <p className="text-lg">{member.role}</p>
-                                <p className="">{member.shortBio}</p>
+                    <div key={member.name} className="bg-neutral-800 rounded-lg">
+                        <div className="flex items-center h-full">
+                            <div className="flex flex-wrap items-center py-4 pl-4 md:flex-nowrap">
+                                {member.photoUrl &&
+                                    <Image src={member.photoUrl} alt={member.name} width={150} height={150} className="rounded-full mb-4 h-40 w-40 md:mb-0 sm:mr-4" />
+                                }
+                                <div className="flex gap-4 mr-4">
+                                    <div className="member-content">
+                                        <h2 className="text-xl font-semibold">{member.name}</h2>
+                                        <p className="text-lg">{member.role}</p>
+                                        <p className="">{member.shortBio}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="flex items-center">
+                            <div className="flex items-center h-full p-4 rounded-r-lg hover:bg-neutral-700 hover:cursor-pointer">
                                 <Link href={`/team/${member.slug}`} className="">
                                     <FontAwesomeIcon icon={faAngleRight} className="font-lg" size="lg" />
                                 </Link>
