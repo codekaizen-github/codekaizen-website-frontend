29204f4da0b7dbd51ba846dabd3436999a7180a7
Missie Dawes <missiedawes@gmail.com>
2024-10-08 21:39:59 +0000

    Update project card and portainer svg

--- src/app/partials/projectCard.tsx
+++ src/app/partials/projectCard.tsx
@@ -11,14 +11,14 @@ interface ProjectCardProps {
 export default function TeamMemberCard({ project }: ProjectCardProps) {
 	return (
 		<div key={project.id} className="bg-neutral-800">
-			<div className="flex items-center h-full">
+			<div className="flex items-center h-full justify-between">
 				<div className="flex flex-wrap items-center py-4 pl-4 md:flex-nowrap">
 					{project.photoUrl && (
 						<Image
 							src={project.photoUrl}
 							alt={project.name}
-							width={80}
-							height={80}
+							width={60}
+							height={60}
 							className="mb-4 md:mb-0 sm:mr-4"
 							priority={true}
 						/>
@@ -33,7 +33,7 @@ export default function TeamMemberCard({ project }: ProjectCardProps) {
 					</div>
 				</div>
 				<Link
-					href={`/team/${project.slug}`}
+					href={`/projects/${project.slug}`}
 					className="flex items-center h-full p-4 hover:bg-neutral-700 hover:cursor-pointer text-retro-blue hover:text-retro-darkBlue"
 				>
 					<FontAwesomeIcon
