37638e9271a33d98f2a7cc583e72ddbf0e2e058f
Missie Dawes <missiedawes@gmail.com>
2024-10-09 19:31:30 +0000

    Update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,24 @@
-export default function NotFound() {
-	// TODO: Improve 404 page
+import Link from "next/link";
 
-	return <div className="">404 - Not found</div>;
+export default function NotFound() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen">
+			<div className="text-center">
+				<h1 className="text-4xl font-bold mb-6">
+					You have strayed into the void.
+				</h1>
+				<p className="mb-2 text-lg">
+					This page has mastered the ancient art of not existing
+					(404).
+				</p>
+				<p className="mb-2 text-lg">
+					Breathe deep, center yourself, and{" "}
+					<Link href="/" className="text-retro-blue">
+						return to the way of the code
+					</Link>
+					.
+				</p>
+			</div>
+		</div>
+	);
 }
