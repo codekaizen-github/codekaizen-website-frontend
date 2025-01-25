875ebd40f50dd799a30efcd65e4ace0a6d0d200e
Missie Dawes <missiedawes@gmail.com>
2025-01-25 21:17:50 +0000

    update env variables

--- src/clients/wordpress/user.ts
+++ src/clients/wordpress/user.ts
@@ -1,7 +1,4 @@
 import { User } from "@interfaces/user";
-import { WordPressUser } from "@interfaces/wpUser";
-
-const baseWPApiUrl = process.env.WORDPRESS_BASE_API_URL;
 
 /**
  * @returns A promise that resolves to an array of User objects
@@ -31,32 +28,4 @@ export async function getAllTeamMemberSlugs(): Promise<string[]> {
 	const data = require("@public/data/teamMembers.json");
 	const slugs = data.map((user: User) => user.slug);
 	return slugs;
-}
-
-export async function getWPUsers(): Promise<WordPressUser[]> {
-	const response = await fetch(baseWPApiUrl + "users");
-	try {
-		if (!response.ok) {
-			throw new Error(`Error fetching users: ${response.statusText}`);
-		}
-		const users: WordPressUser[] = await response.json();
-		return users;
-	} catch (error) {
-		console.error("Failed to fetch team members:", error);
-		throw error;
-	}
-}
-
-export async function getWPUsersById(id: number): Promise<WordPressUser> {
-	const response = await fetch(baseWPApiUrl + "users/" + id);
-	try {
-		if (!response.ok) {
-			throw new Error(`Error fetching users: ${response.statusText}`);
-		}
-		const user: WordPressUser = await response.json();
-		return user;
-	} catch (error) {
-		console.error("Failed to fetch team members:", error);
-		throw error;
-	}
 }
