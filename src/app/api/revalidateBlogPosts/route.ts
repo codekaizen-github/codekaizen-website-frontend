99b29f9f3bb0339af290965db90446606f99f2bb
Missie Dawes <missiedawes@gmail.com>
2025-01-25 18:17:56 +0000

    add bearer auth to revalidate endpoint

--- src/app/api/revalidateBlogPosts/route.ts
+++ src/app/api/revalidateBlogPosts/route.ts
@@ -1,7 +1,16 @@
 import { getAllBlogPostSlugs } from "@/clients/wordpress/postSlug";
 import { revalidatePath } from "next/cache";
 
+const VALID_TOKEN = process.env.REVALIDATE_SECRET;
+
 export async function GET(req: Request) {
+	const token = req.headers.get("Authorization");
+	if (token !== `Bearer ${VALID_TOKEN}`) {
+		return new Response("Unauthorized", {
+			status: 401,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
 	try {
 		await getAllBlogPostSlugs();
 		revalidatePath("/blog");
