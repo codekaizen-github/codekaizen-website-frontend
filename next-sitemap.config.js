390f24f2fdeb9dbe57078a424dee442989a711a7
Missie Dawes <missiedawes@gmail.com>
2025-04-05 20:06:57 +0000

    Remove next-sitemap stuff

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	siteUrl: "https://codekaizen.net",
-	generateRobotsTxt: true, // Generates a robots.txt file as well
-	sitemapSize: 5000, // Optional: specify max URLs per sitemap file
-	changefreq: "daily", // Optional: frequency of page updates
-	//priority: 0.7, // Optional: default priority for URLs
-	//exclude: ["/admin/*", "/private/*"],
-};
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: [process.env.WORDPRESS_HOST_NAME],
-    },
-};
-
-export default nextConfig;
